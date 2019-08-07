6f000fd854eeac23e0ba60df5e21735e16789a4b
Syris Norelli <snore001@ucr.edu>
2019-08-07 13:11:24 -0700

    first commit

---
+++ data/conv_to_json.py
@@ -0,0 +1,27 @@
+import json
+import csv
+
+# Methodology:
+# 1. Write up eco on Google Sheets
+# 2. Put the CSV in the ~/data/SkyblockOriginal folder, call it ecoinfo.csv
+# 3. This code will convert it to json
+
+def convertCSV(server_name: str) -> None:
+    outJSON = dict()
+    subfolder = './' + server_name + '/'
+    with open(subfolder + 'ecoinfo.csv', 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if row[0] != 'Shop Prices' and row[0] != 'Resource':
+                row = [row[0]] + [(float(x) if x != '' else None) for x in row[1:]]
+                outJSON[row[0]] = {
+                    'Buy Price': row[1],
+                    'Sell Price': row[2],
+                    'Automation State': row[3]
+                }
+    with open(subfolder + 'ecoinfo.json', 'w') as f:
+        json.dump(outJSON, f, indent=4)
+
+if __name__ == '__main__':
+    server_name = 'SkyblockOriginal'
+    convertCSV(server_name)
---
+++ data/grabcraftingrecipes.py
@@ -0,0 +1,81 @@
+from lxml import html
+import requests
+import json
+
+url = 'http://diamondboots.com/#crafting' # Version 1.12
+
+GET_DATA = False
+
+if GET_DATA:
+    page = requests.get(url)
+
+    with open('test.html', 'w') as f:
+        f.write(page.content.decode('utf-8'))
+
+# Line 228 is the interesting one (1-indexed)
+with open('test.html', 'r') as f:
+    recipe_data = f.readlines()[227]
+    tree = html.fromstring(recipe_data)
+
+test_set = [x for x in tree]
+# print(test_set)
+# Wanted: names, crafting recipes
+
+## ITEM DATABASE IDs
+IDs = [x.get('id')[5:] for x in test_set]
+
+### NAMES
+def strongTolerant(elt):
+    endStr = elt.text
+    strong_text = elt.xpath('.//strong')
+    if strong_text:
+        endStr += ''.join([x.text for x in strong_text])
+    return endStr
+
+names = [strongTolerant(x) for x in tree.xpath('.//h2')]
+
+### CRAFTING RECIPES
+recipes = [x[1].get('recipe') for x in test_set]
+
+### ALL TOGETHER NOW
+ID_lookup = {i: {
+                    'name': n,
+                    'recipe': r if r is None else r.split(',')
+                }
+                for i, n, r in zip(IDs, names, recipes)}
+
+### CONVERT RECIPES
+
+ID_lookup['424']['recipe'] = '423,263' # One edge case for some reason
+
+for key, subdict in ID_lookup.items():
+    for name, recipe in subdict.items():
+        if isinstance(recipe, list):
+            if len(recipe) > 0:
+                # Get names of all nonzero items
+                newdict = dict()
+                newdictkeys = []
+                for ingredient in recipe:
+                    if ingredient not in ['0', '']:
+                        try:
+                            iname = ID_lookup[ingredient]['name']
+                        except KeyError:
+                            try: # Another edge case
+                                iname = ID_lookup['-' + ingredient]['name']
+                            except KeyError:
+                                print(key, name, recipe)
+                                raise
+                        if iname in newdictkeys:
+                            newdict[iname] += 1
+                        else:
+                            newdict[iname] = 1
+                            newdictkeys.append(iname)
+                ID_lookup[key]['recipe'] = newdict
+
+findict = dict()
+for key, subdict in ID_lookup.items():
+    if subdict['recipe'] is not None:
+        findict[subdict['name']] = subdict['recipe']
+
+with open('recipes.json', 'w') as f:
+    json.dump(findict, f, indent=4)
